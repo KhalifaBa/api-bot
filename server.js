cffa2fef71f8b5d60956c29a71bc6b9ed51450d4
unknown <khalifalo178@gmail.com>
2025-07-30 12:16:30 +0200

    add randomMove

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ const PORT = process.env.PORT || 3000;
 const move = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"];
 const action = ["BOMB", "COLLECT","ATTACK", "NONE"];
 
+const randomMove = Math.floor(Math.random() * move.length);
+const randomAction = Math.floor(Math.random() * action.length);
+
 app.listen(PORT, () => {
   console.log("Serveur sur écoute :", PORT);
 });
 
 app.get("/action", (request, response) => {
    const bot = {
-      "move": "",
-      "action": ""
+      "move": randomMove,
+      "action": randomAction
    };
    
    response.send(bot);
